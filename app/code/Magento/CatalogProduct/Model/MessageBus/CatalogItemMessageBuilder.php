a86c545c374d4a8a317dff12a563158fdddbc664
Michail Slabko <mslabko@magento.com>
2020-01-15 09:24:04 -0600

    MC-29551: Storage Reader

--- app/code/Magento/CatalogProduct/Model/MessageBus/CatalogItemMessageBuilder.php
+++ app/code/Magento/CatalogProduct/Model/MessageBus/CatalogItemMessageBuilder.php
@@ -43,7 +43,8 @@ class CatalogItemMessageBuilder
      * @param array $message
      * @return CatalogItemMessage
      */
-    public function build($message): CatalogItemMessage {
+    public function build($message): CatalogItemMessage
+    {
         $message = $this->serializer->unserialize($message);
         $this->validateMessage($message);
 
