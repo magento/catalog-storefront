606fee3d505d476525589b953a9d3ea3da7bb809
Ruslan Kostiv <rkostiv@adobe.com>
2020-01-29 16:24:40 -0600

    MC-23332: Layered navigation moving to SF

--- app/code/Magento/CatalogProduct/Model/ProductSearch.php
+++ app/code/Magento/CatalogProduct/Model/ProductSearch.php
@@ -83,7 +83,7 @@ class ProductSearch implements ProductSearchInterface
         }
         $productIds = (array)$criteria->getFilters()['ids'];
         if (!$productIds) {
-            throw new \InvalidArgumentException(_('Currently Catalog Storefront service supports only product ids'));
+            throw new \InvalidArgumentException('Currently Catalog Storefront service supports only product ids');
         }
 
         $productItems = $this->dataProvider->fetch(
