2521b514850615ca9a15a6cf274c52c29812ce3f
Leonid Poluianov <poluyano@addobe.com>
2020-03-09 15:02:19 -0500

    Redudant image transforming fixed

--- app/code/Magento/CatalogExtractor/DataProvider/MediaGallery/ProductImageTransformer.php
+++ app/code/Magento/CatalogExtractor/DataProvider/MediaGallery/ProductImageTransformer.php
@@ -51,14 +51,16 @@ class ProductImageTransformer implements TransformerInterface
         }
 
         foreach ($productItems as &$item) {
-            $rawValue = $item[$attributeName] ?? '';
-            $item[$attributeName] = [];
+            if (isset($item[$attributeName])) {
+                $rawValue = $item[$attributeName] ?? '';
+                $item[$attributeName] = [];
 
-            if (\in_array('url', $fields, true)) {
-                $item[$attributeName]['url'] = $this->imageUrlResolver->resolve($rawValue, $attributeName);
-            }
-            if (\in_array('label', $fields, true)) {
-                $item[$attributeName]['label'] = $item[$attributeName . '_label'] ?? $item['name'] ?? '';
+                if (\in_array('url', $fields, true)) {
+                    $item[$attributeName]['url'] = $this->imageUrlResolver->resolve($rawValue, $attributeName);
+                }
+                if (\in_array('label', $fields, true)) {
+                    $item[$attributeName]['label'] = $item[$attributeName . '_label'] ?? $item['name'] ?? '';
+                }
             }
         }
 
