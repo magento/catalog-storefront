aaa0f21901de081bd7b426ebd1327bae0b7f3ef5
Andrii Konosov <akonosov@adobe.com>
2020-06-05 16:32:38 -0500

    ECP-531: Fix current data sync bugs - Introduced generic object convertor

---
+++ app/code/Magento/CatalogStorefrontGraphQl/Model/Converter/ObjectToArray.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Magento\CatalogStorefrontGraphQl\Model\Converter;
+
+use Magento\Catalog\Api\Data\CategoryInterface;
+use Magento\Catalog\Api\CategoryRepositoryInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+/**
+ * Provides array based on provided object
+ *
+ * Extracts array_keys from getters and convert camel case to snake case
+ * (e.g. Object::getAttributeCode -> [attribute_code => value])
+ */
+class ObjectToArray
+{
+    private $reflectionCache = [];
+
+    /**
+     * Extract data from provided object and convert it to a new array
+     * Optional map argument allows to map specific methods to array keys
+     *
+     * @param $object - php object
+     * @param array $map
+     * @return array
+     */
+    public function getArray($object, array $map = []): array
+    {
+        if (!is_object($object)) {
+            throw new \InvalidArgumentException('First argument must be an object');
+        }
+        $map = $this->fetchMap($object, $map);
+        $result = [];
+        foreach ($map as $entry) {
+            $method = $entry['method'];
+            $value = $object->$method();
+            if (is_object($value)) {
+                $value = $this->getArray($value);
+            }
+            if (is_array($value) && !empty($value) && is_object(reset($value))) {
+                $arrayValue = [];
+                foreach ($value as $item) {
+                    $arrayValue[] = $this->getArray($item, $map);
+                }
+                $value = $arrayValue;
+            }
+            $result[$entry['key']] = $value;
+        }
+        return $result;
+    }
+
+    /**
+     * Extracts default mapping from the object using reflection
+     * Method also applies overrides specified in the map.
+     *
+     * @param $object
+     * @return array [method => array key]
+     */
+    private function fetchMap($object, array $mapOverride = []): array
+    {
+        if (isset($this->reflectionCache[get_class($object)])) {
+            return $this->reflectionCache[get_class($object)];
+        }
+        $overrides = [];
+        foreach ($mapOverride as $interface => $interfaceMap) {
+            if (!in_array($interface, class_implements($object))) {
+                continue;
+            }
+            foreach ($interfaceMap as $method => $key) {
+                $overrides[$method] = $key;
+            }
+        }
+        $result = [];
+        $dataObjectMethods = get_class_methods(get_class($object));
+        foreach ($dataObjectMethods as $method) {
+            if (0 !== strpos($method, 'get')) {
+                //The method is not a getter
+                continue;
+            }
+
+            $result[] = [
+                'method' => $method,
+                'key' => $overrides[$method] ?? preg_replace(
+                    '~^get_(.*)$~',
+                    '$1',
+                    strtolower(preg_replace('~([A-Z])~', '_$1', $method))
+                )
+            ];
+        }
+        $this->reflectionCache[get_class($object)] = $result;
+        return $result;
+    }
+
+}
--- app/code/Magento/CatalogStorefrontGraphQl/Resolver/Product/OutputFormatter.php
+++ app/code/Magento/CatalogStorefrontGraphQl/Resolver/Product/OutputFormatter.php
@@ -22,6 +22,7 @@ use Magento\CatalogStorefrontApi\Api\Data\UrlRewriteInterface;
 use Magento\CatalogStorefrontApi\Api\Data\UrlRewriteParameterInterface;
 use Magento\CatalogStorefrontApi\Api\Data\VariantAttributeInterface;
 use Magento\CatalogStorefrontApi\Api\Data\VariantInterface;
+use Magento\CatalogStorefrontGraphQl\Model\Converter\ObjectToArray;
 use Magento\Framework\GraphQl\Exception\GraphQlInputException;
 use Magento\Framework\GraphQl\Query\Resolver\BatchRequestItemInterface;
 
@@ -53,253 +54,32 @@ class OutputFormatter
             //TODO: change with error holder
             throw new GraphQlInputException(__(\implode('; ', \array_map('\strval', $errors))));
         }
+        $converter = new ObjectToArray;
+        $items = [];
+        foreach ($result->getItems() as $item) {
+            $currentResult = $converter->getArray($item);
+            $currentResult['entity_id'] = $currentResult['id'];
+            $currentResult['description'] = ['html' => $currentResult['description']];
+            $currentResult['short_description'] = ['html' => $currentResult['short_description']];
+            $currentResult['gift_message_available'] = (int)$currentResult['gift_message_available'];
+            $currentResult['canonical_url'] = empty($currentResult['canonical_url'])
+                ? null
+                : $currentResult['canonical_url'];
 
-        $items = array_map(function (\Magento\CatalogStorefrontApi\Api\Data\ProductInterface $item) {
-            $result = [
-                'attribute_set_id' => $item->getAttributeSetId(),
-                'categories' => $item->getCategories(),
-                'created_at' => $item->getCreatedAt(),
-                'updated_at' => $item->getUpdatedAt(),
-                'sku' => $item->getSku(),
-                'id' => $item->getId(),
-                'entity_id' => $item->getId(),
-                'type_id' => $item->getTypeId(),
-                'description' => ['html' => $item->getDescription() ?? null],
-                'short_description' => ['html' => $item->getShortDescription() ?? null],
-                'name' => $item->getName(),
-                'stock_status' => $item->getStockStatus(),
-                'url_key' => $item->getUrlKey(),
-                'url_suffix' => $item->getUrlSuffix(),
-                'swatch_image' => $item->getSwatchImage(),
-                'weight' => $item->getWeight(),
-                'meta_description' => $item->getMetaDescription(),
-                'meta_keyword' => $item->getMetaKeyword(),
-                'meta_title' => $item->getMetaTitle(),
-                'country_of_manufacture' => $item->getCountryOfManufacture(),
-                'gift_message_available' => (int)$item->getGiftMessageAvailable(),
-                'options_container' => $item->getOptionsContainer(),
-                'special_price' => $item->getSpecialPrice(),
-                'special_from_date' => $item->getSpecialFromDate(),
-                'special_to_date' => $item->getSpecialToDate(),
-                'canonical_url' => empty($item->getCanonicalUrl()) ? null : $item->getCanonicalUrl(),
-                'ship_bundle_items' => $item->getShipBundleItems(),
-                'dynamic_weight' => $item->getDynamicWeight(),
-                'dynamic_sku' => $item->getDynamicSku(),
-                'dynamic_price' => $item->getDynamicPrice(),
-                'price_view' => $item->getPriceView(),
-            ];
-
-            if ($item->getImage()) {
-                $result['image']['url'] = $item->getImage()->getUrl() ?? "";
-                $result['image']['label'] = $item->getImage()->getLabel() ?? "";
-            }
-
-            if ($item->getSmallImage()) {
-                $result['small_image']['url'] = $item->getSmallImage()->getUrl() ?? "";
-                $result['small_image']['label'] = $item->getSmallImage()->getLabel() ?? "";
-            }
-
-            if ($item->getThumbnail()) {
-                $result['thumbnail']['url'] = $item->getThumbnail()->getUrl() ?? "";
-                $result['thumbnail']['label'] = $item->getThumbnail()->getLabel() ?? "";
-            }
-
-            foreach ($item->getMediaGallery() as $offset => $galleryItem) {
-                $galleryItemOutput = [
-                    'position' => $galleryItem->getPosition(),
-                    'url' => $galleryItem->getUrl(),
-                    'label' => $galleryItem->getLabel(),
-                    'media_type' => $galleryItem->getMediaType(),
-                    'disabled' => false
-                ];
-                if ($galleryItem->getVideoContent()) {
-                    $galleryItemOutput['video_content'] = [
-                        'media_type' => $galleryItem->getVideoContent()->getMediaType(),
-                        'video_description' => $galleryItem->getVideoContent()->getVideoDescription(),
-                        'video_metadata' => $galleryItem->getVideoContent()->getVideoMetadata(),
-                        'video_provider' => $galleryItem->getVideoContent()->getVideoProvider(),
-                        'video_title' => $galleryItem->getVideoContent()->getVideoTitle(),
-                        'video_url' => $galleryItem->getVideoContent()->getVideoUrl(),
-                    ];
+            foreach ($currentResult['options'] as &$option) {
+                //Convert simple option types from arrays
+                $simpleOptionTypes = ['date_time', 'field', 'area', 'file'];
+                if (isset($option['type']) && in_array($option['type'], $simpleOptionTypes)) {
+                    $option['value'] = reset($option['value']);
                 }
-                $result['media_gallery'][] = $galleryItemOutput;
             }
+            $currentResult['media_gallery'] = array_map(function (array $item) {
+                $item['disabled'] = false;
+                return $item;
+            }, $currentResult['media_gallery']);
 
-            //TODO: Revise options structure and remove free form structure from index/api
-            if ($item->getOptions()) {
-                $result['options'] = array_map(function (OptionInterface $option) {
-                    $output = [
-                        'option_id' => $option->getOptionId(),
-                        'product_id' => $option->getProductId(),
-                        'type' => $option->getType(),
-                        'is_require' => $option->getIsRequire(),
-                        'sku' => $option->getSku(),
-                        'max_characters' => (int)$option->getMaxCharacters(),
-                        'file_extension' => $option->getFileExtension(),
-                        'image_size_x' => (int)$option->getImageSizeX(),
-                        'image_size_y' => (int)$option->getImageSizeY(),
-                        'sort_order' => $option->getSortOrder(),
-                        'default_title' => $option->getDefaultTitle(),
-                        'store_title' => $option->getStoreTitle(),
-                        'title' => $option->getTitle(),
-                        'default_price' => $option->getDefaultPrice(),
-                        'default_price_type' => $option->getDefaultPriceType(),
-                        'store_price' => $option->getStorePrice(),
-                        'store_price_type' => $option->getStorePriceType(),
-                        'price' => (float)$option->getPrice(),
-                        'price_type' => empty($option->getPriceType()) ? "FIXED" : $option->getPriceType(),
-                        'required' => $option->getRequired(),
-                        'product_sku' => $option->getProductSku(),
-                    ];
-                    $output['value'] = array_map(function (OptionValueInterface $value) {
-                        return [
-                            'option_id' => $value->getOptionId(),
-                            'option_type_id' => $value->getOptionTypeId(),
-                            'product_id' => $value->getProductId(),
-                            'type' => $value->getType(),
-                            'is_require' => $value->getIsRequire(),
-                            'sku' => $value->getSku(),
-                            'max_characters' => (int)$value->getMaxCharacters(),
-                            'file_extension' => $value->getFileExtension(),
-                            'image_size_x' => (int)$value->getImageSizeX(),
-                            'image_size_y' => (int)$value->getImageSizeY(),
-                            'sort_order' => $value->getSortOrder(),
-                            'default_title' => $value->getDefaultTitle(),
-                            'store_title' => $value->getStoreTitle(),
-                            'title' => $value->getTitle(),
-                            'default_price' => $value->getDefaultPrice(),
-                            'default_price_type' => $value->getDefaultPriceType(),
-                            'store_price' => $value->getStorePrice(),
-                            'store_price_type' => $value->getStorePriceType(),
-                            'price' => (float)$value->getPrice(),
-                            'price_type' => empty($value->getPriceType()) ? "FIXED" : $value->getPriceType(),
-
-                        ];
-                    }, $option->getValue());
-
-                    //Convert simple option types from arrays
-                    $simpleOptionTypes = ['date_time', 'field', 'area', 'file'];
-                    if (isset($output['type']) && in_array($output['type'], $simpleOptionTypes)) {
-                        $output['value'] = reset($output['value']);
-                    }
-
-                    return $output;
-                }, $item->getOptions());
-            }
-
-            $result['product_links'] = array_map(function (ProductLinkInterface $item) {
-                return [
-                    "linked_product_sku" => $item->getLinkedProductSku(),
-                    "linked_product_type" => $item->getLinkedProductType(),
-                    "link_type_id" => $item->getLinkTypeId(),
-                    "position" => $item->getPosition(),
-                    "sku" => $item->getSku(),
-                    "link_type" => $item->getLinkType(),
-                ];
-            }, $item->getProductLinks());
-            if (!empty($item->getVariants())) {
-                $result['variants'] = array_map(function ($item) {
-                    $output = [
-                        'product' => $item->getProduct(),
-                        'attributes' => array_map(function ($item) {
-                            return [
-                                'label' => $item->getLabel(),
-                                'code' => $item->getCode(),
-                                'value_index' => $item->getValueIndex()
-                            ];
-                        }, $item->getAttributes())
-                    ];
-                    return $output;
-                }, $item->getVariants());
-            }
-
-            $result['url_rewrites'] = array_map(function (UrlRewriteInterface $item) {
-                $parameters = array_map(function (UrlRewriteParameterInterface $item) {
-                    return [
-                        'name' => $item->getName(),
-                        'value' => $item->getValue(),
-                    ];
-                }, $item->getParameters());
-                return [
-                    'url' => $item->getUrl(),
-                    'parameters' => $parameters
-                ];
-            }, $item->getUrlRewrites());
-
-            if ($item->getItems()) {
-                $result['items'] = array_map(function (BundleItemInterface $item) {
-                    $options = array_map(function (BundleItemOptionInterface $item) {
-                        return [
-                            'id' => $item->getId(),
-                            'label' => $item->getLabel(),
-                            //'product' => ['id' => $item->getEntityId(), 'type_id' => 'simple'],
-                            'product' => $item->getEntityId(),
-                            'can_change_quantity' => $item->getCanChangeQuantity(),
-                            'is_default' => $item->getIsDefault(),
-                            'price' => $item->getPrice(),
-                            'price_type' => $item->getPriceType(),
-                            'quantity' => $item->getQuantity(),
-                            'position' => $item->getPosition(),
-                        ];
-                    }, $item->getOptions());
-                    return [
-                        'position' => $item->getPosition(),
-                        'sku' => $item->getSku(),
-                        'type' => $item->getType(),
-                        'option_id' => $item->getOptionId(),
-                        'required' => $item->getRequired(),
-                        'title' => $item->getTitle(),
-                        'options' => $options
-                    ];
-                }, $item->getItems());
-            }
-
-            if ($item->getConfigurableOptions()) {
-                $result['configurable_options'] = array_map(function(ConfigurableOptionInterface $item) {
-                    $values = array_map(function (ConfigurableOptionValueInterface $item) {
-                        return [
-                            'attribute_id' => $item->getAttributeId(),
-                            'product_id' => $item->getProductId(),
-                            'label' => $item->getLabel(),
-                            'value_index' => $item->getValueIndex(),
-                            'default_label' => $item->getDefaultLabel(),
-                            'store_label' => $item->getStoreLabel(),
-                            'use_default_value' => $item->getUseDefaultValue(),
-                        ];
-                    }, $item->getValues());
-                    return [
-                        'label' => $item->getLabel(),
-                        'id' => $item->getId(),
-                        'position' => $item->getPosition(),
-                        'product_id' => $item->getProductId(),
-                        'attribute_code' => $item->getAttributeCode(),
-                        'attribute_id' => $item->getAttributeId(),
-                        'use_default' => $item->getUseDefault(),
-                        'values' => $values
-                    ];
-                }, $item->getConfigurableOptions());
-            }
-
-            if ($item->getVariants()) {
-                $result['variants'] = array_map(function (VariantInterface $item) {
-                    $attributes = array_map(function (VariantAttributeInterface $item) {
-                        return [
-                            'value_index' => $item->getValueIndex(),
-                            'label' => $item->getLabel(),
-                            'code' => $item->getCode(),
-                        ];
-                    }, $item->getAttributes());
-                    return [
-                        'product' => $item->getProduct(),
-                        'attributes' => $attributes
-                    ];
-                }, $item->getVariants());
-            }
-
-            return $result;
-        }, $result->getItems());
-
-
+            $items[] = $currentResult;
+        }
         $metaInfo = $additionalInfo['meta_info'] ?? [];
         $aggregations = $additionalInfo['aggregations'] ?? [];
 
