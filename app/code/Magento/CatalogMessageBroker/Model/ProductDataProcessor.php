02051ee6b0209957c40435ad7b140b6d21325210
ruslankostiv <rkostiv@adobe.com>
2020-06-22 19:46:48 -0500

    #18:  Backoffice APIs Message broker syncs product custom options from PIM-storefront

--- app/code/Magento/CatalogMessageBroker/Model/ProductDataProcessor.php
+++ app/code/Magento/CatalogMessageBroker/Model/ProductDataProcessor.php
@@ -35,25 +35,25 @@ class ProductDataProcessor
     /**
      * Override data returned from old API with data returned from new API
      *
-     * @param array $override
+     * @param array $data
      * @param array $product
      * @return array
      * @deprecated this is a temporary solution that will be replaced
      * with declarative schema of mapping exported data format to storefront format
      */
-    public function merge(array $override, array $product): array
+    public function merge(array $data, array $product): array
     {
         $overriddenFields = [];
 
         foreach ($this->fields as $field) {
-            if (isset($override[$field])) {
-                $overriddenFields[$field] = $override[$field];
+            if (isset($data[$field])) {
+                $overriddenFields[$field] = $data[$field];
             }
         }
 
         /** @var DataMapperInterface $dataMapper */
         foreach ($this->dataMappers as $field => $dataMapper) {
-            $overriddenFields[$field] = $dataMapper->map($override);
+            $overriddenFields[$field] = $dataMapper->map($data);
         }
 
         return array_merge($product, $overriddenFields);
