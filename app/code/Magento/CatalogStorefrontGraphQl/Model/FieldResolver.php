269194f4852714785f39d0cdffb828f0c98f9778
ruslankostiv <rkostiv@adobe.com>
2020-07-14 10:37:28 -0500

    #146: Automation tests failed after merge with mainline - CategoryListTest.testFilterCategoryNamedFragment failure fixed

--- app/code/Magento/CatalogStorefrontGraphQl/Model/FieldResolver.php
+++ app/code/Magento/CatalogStorefrontGraphQl/Model/FieldResolver.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Magento\CatalogStorefrontGraphQl\Model;
 
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use GraphQL\Language\AST\NodeKind;
 
 /**
  * Resolve fields for query.
@@ -54,7 +55,7 @@ class FieldResolver
                 }
 
                 if (!isset($selection->selectionSet, $selection->selectionSet->selections)) {
-                    $fieldNames = $this->getFieldNames($selection, $fieldNames);
+                    $fieldNames = $this->getFieldNames($selection, $fieldNames, (array)$info->fragments);
                 } else {
                     if (null !== $requestedField && $selection->name->value == $requestedField) {
                         $fieldNames = $this->getFieldNames($selection, []);
@@ -76,28 +77,34 @@ class FieldResolver
      *
      * @param \GraphQL\Language\AST\SelectionNode $selection
      * @param array $fieldNames
+     * @param \GraphQL\Language\AST\FragmentDefinitionNode[] $fragments
      * @return array
      * @SuppressWarnings(PHPMD.CyclomaticComplexity)
      */
-    private function getFieldNames(\GraphQL\Language\AST\SelectionNode $selection, array $fieldNames): array
-    {
+    private function getFieldNames(
+        \GraphQL\Language\AST\SelectionNode $selection,
+        array $fieldNames,
+        $fragments = []
+    ): array {
         if (!isset($selection->selectionSet) && !isset($selection->selectionSet->selections)) {
-            if ($selection->kind === 'Field' && $selection->name->value) {
+            if ($selection->kind === NodeKind::FIELD && $selection->name->value) {
                 $fieldNames[] = $selection->name->value;
+            } elseif ($selection->kind === NodeKind::FRAGMENT_SPREAD && !empty($fragments)) {
+                $fieldNames = $this->addFragmentFields($fieldNames, $fragments);
             }
             return $fieldNames;
         }
 
         foreach ($selection->selectionSet->selections as $itemSelection) {
-            if ($itemSelection->kind === 'InlineFragment') {
+            if ($itemSelection->kind === NodeKind::INLINE_FRAGMENT) {
                 foreach ($itemSelection->selectionSet->selections as $inlineSelection) {
-                    if ($itemSelection->typeCondition->kind === 'NamedType') {
+                    if ($itemSelection->typeCondition->kind === NodeKind::NAMED_TYPE) {
                         $namedType = $itemSelection->typeCondition->name->value;
                         $fieldNames = $this->getNestedFields($fieldNames, $inlineSelection, $namedType);
                         continue;
                     }
 
-                    if ($inlineSelection->kind === 'InlineFragment') {
+                    if ($inlineSelection->kind === NodeKind::INLINE_FRAGMENT) {
                         continue;
                     }
                     $fieldNames = $this->getNestedFields($fieldNames, $inlineSelection);
@@ -107,6 +114,26 @@ class FieldResolver
 
             $fieldNames = $this->getNestedFields($fieldNames, $itemSelection);
         }
+
+        return $fieldNames;
+    }
+
+    /**
+     * @param array $fieldNames
+     * @param array $fragments
+     */
+    private function addFragmentFields(array $fieldNames, array $fragments)
+    {
+        $fragmentFieldNames = [];
+        /** @var \GraphQL\Language\AST\FragmentDefinitionNode $fragment */
+        foreach ($fragments as $fragment) {
+            /** @var \GraphQL\Language\AST\FieldNode $itemSelection */
+            foreach ($fragment->selectionSet->selections as $itemSelection) {
+                $fragmentFieldNames []= $this->getNestedFields($fieldNames, $itemSelection);
+            }
+        }
+
+        $fieldNames = array_merge($fieldNames, ...$fragmentFieldNames);
 
         return $fieldNames;
     }
