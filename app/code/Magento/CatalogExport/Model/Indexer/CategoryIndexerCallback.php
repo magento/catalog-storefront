82af658385bd352429983e198ec5046eeffde028
Serhii Kovalenko <ganster3012@gmail.com>
2020-07-17 19:18:49 +0300

    [#25 - Parent] Message broker :: Categories :: Sync category data from back-office/PIM #25
    --debug to identify problems with messagebroker

--- app/code/Magento/CatalogExport/Model/Indexer/CategoryIndexerCallback.php
+++ app/code/Magento/CatalogExport/Model/Indexer/CategoryIndexerCallback.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 namespace Magento\CatalogExport\Model\Indexer;
 
 use Magento\CatalogDataExporter\Model\Indexer\CategoryIndexerCallbackInterface;
+use Magento\CatalogMessageBroker\Model\MessageBus\CategoriesConsumer;
 use Magento\Framework\MessageQueue\PublisherInterface;
 use Psr\Log\LoggerInterface;
 
@@ -29,17 +30,24 @@ class CategoryIndexerCallback implements CategoryIndexerCallbackInterface
      * @var LoggerInterface
      */
     private $logger;
+    /**
+     * @var CategoriesConsumer
+     */
+    private $categoriesConsumer;
 
     /**
      * @param PublisherInterface $queuePublisher
+     * @param CategoriesConsumer $categoriesConsumer
      * @param LoggerInterface $logger
      */
     public function __construct(
         PublisherInterface $queuePublisher,
+        CategoriesConsumer $categoriesConsumer,
         LoggerInterface $logger
     ) {
         $this->queuePublisher = $queuePublisher;
         $this->logger = $logger;
+        $this->categoriesConsumer = $categoriesConsumer;
     }
 
     /**
@@ -50,6 +58,7 @@ class CategoryIndexerCallback implements CategoryIndexerCallbackInterface
         foreach (array_chunk($ids, self::BATCH_SIZE) as $idsChunk) {
             if (!empty($idsChunk)) {
                 try {
+                    $this->categoriesConsumer->processMessage(json_encode($idsChunk));
                     // @todo understand why string[] doesn't work
                     $this->queuePublisher->publish(self::TOPIC_NAME, json_encode($idsChunk));
                 } catch (\Exception $e) {
