8a72db14bd6e4d880a82abafd2a371b673e73b1e
ruslankostiv <rkostiv@adobe.com>
2020-08-10 18:43:34 -0500

    #111: Expose configurable product attributes data via export API

--- dev/tests/api-functional/testsuite/Magento/CatalogExport/Api/DynamicAttributesTest.php
+++ dev/tests/api-functional/testsuite/Magento/CatalogExport/Api/DynamicAttributesTest.php
@@ -112,19 +112,10 @@ class DynamicAttributesTest extends WebapiAbstract
     {
         if (is_array($expected)) {
             $actual = !is_array($actual) ? json_decode($actual, true) : $actual;
-            $this->assertEquals(
-                sizeof($expected),
-                sizeof($actual),
-                'Expected and actual are of different size, expected '
-                . json_encode($expected)
-                . ', actual '
-                . json_encode($actual)
-                . '.'
-            );
             foreach (array_keys($expected) as $key) {
                 $snakeCaseKey = $this->camelToSnakeCase($key);
                 $this->assertTrue(
-                    isset($actual[$snakeCaseKey]),
+                    \array_key_exists($snakeCaseKey, $actual),
                     $snakeCaseKey . 'doesn\'t exist, '
                     . json_encode($expected)
                     . ', actual '
