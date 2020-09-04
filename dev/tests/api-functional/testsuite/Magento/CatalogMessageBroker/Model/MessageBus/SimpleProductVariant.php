36e6ef0747bcea10deac7827b20e9ecc9e796ee0
LedianHymetllari <ledian.hymetllari@gmail.com>
2020-09-04 15:26:59 +0100

    added test class

---
+++ dev/tests/api-functional/testsuite/Magento/CatalogMessageBroker/Model/MessageBus/SimpleProductVariant.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\CatalogMessageBroker\Model\MessageBus;
+
+use Magento\TestFramework\TestCase\StorefrontTestsAbstract;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\CatalogStorefront\Model\CatalogService;
+use Magento\CatalogStorefrontApi\Api\Data\ProductsGetRequestInterface;
+use Magento\CatalogStorefrontApi\Api\Data\ProductShopperInputOptionArrayMapper;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\Helper\CompareArraysRecursively;
+
+/**
+ * Test class for ProductVariants message bus
+ *
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class SimpleProductVariant extends StorefrontTestsAbstract
+{
+    const STORE_CODE = 'default';
+
+    /**
+     * @var string[]
+     */
+    private $attributesToCompare = [
+        'shopper_input_options'
+    ];
+
+    /**
+     * @var CatalogService
+     */
+    private $catalogService;
+
+    /**
+     * @var ProductsGetRequestInterface
+     */
+    private $productsGetRequestInterface;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected $productRepository;
+
+    /**
+     * @var FeedInterface
+     */
+    private $productFeed;
+
+    /**
+     * @var Json
+     */
+    private $json;
+
+    /**
+     * @inheritdoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->catalogService = Bootstrap::getObjectManager()->create(CatalogService::class);
+        $this->productsGetRequestInterface = Bootstrap::getObjectManager()->create(ProductsGetRequestInterface::class);
+        $this->productRepository = Bootstrap::getObjectManager()->create(ProductRepositoryInterface::class);
+    }
+
+    /**
+     * Test product export REST API
+     *
+     * @magentoApiDataFixture Magento/Catalog/_files/product_simple_with_custom_attribute.php
+     * @throws \Throwable
+     */
+    public function testStoreFrontData(): void
+    {
+        $product = $this->productRepository->get('simple');
+        
+
+        $this->productsGetRequestInterface->setIds([$product->getId()]);
+        $this->productsGetRequestInterface->setStore(self::STORE_CODE);
+        $this->productsGetRequestInterface->setAttributeCodes($this->attributesToCompare);
+        $catalogServiceItem = $this->catalogService->getProducts($this->productsGetRequestInterface);
+
+        $this->assertNotEmpty($catalogServiceItem->getItems());
+        $item = $catalogServiceItem->getItems()[0];
+        var_dump($item);
+
+    }
+}
