c867e8decf73e6ca5a41645c615c973dd190e322
LedianHymetllari <ledian.hymetllari@gmail.com>
2020-09-15 21:21:27 +0100

    added test class

--- app/code/Magento/CatalogStorefront/Test/Api/Product/Configurable/ConfigurableProductOptionsTest.php
+++ app/code/Magento/CatalogStorefront/Test/Api/Product/Configurable/ConfigurableProductOptionsTest.php
@@ -13,6 +13,8 @@ use Magento\CatalogStorefront\Test\Api\StorefrontTestsAbstract;
 use Magento\CatalogStorefrontApi\Api\Data\ProductsGetRequestInterface;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\TestFramework\Helper\Bootstrap;
+use Magento\TestFramework\Helper\CompareArraysRecursively;
+use Magento\CatalogStorefrontApi\Api\Data\ConfigurableOptionValueArrayMapper;
 
 class ConfigurableProductOptionsTest extends StorefrontTestsAbstract
 {
@@ -20,6 +22,7 @@ class ConfigurableProductOptionsTest extends StorefrontTestsAbstract
      * Test Constants
      */
     const TEST_SKU = 'configurable';
+    const TWO_CHILD_SKU = 'Configurable product';
     const STORE_CODE = 'default';
 
     /**
@@ -45,6 +48,16 @@ class ConfigurableProductOptionsTest extends StorefrontTestsAbstract
     private $productRepository;
 
     /**
+     * @var CompareArraysRecursively
+     */
+    private $compareArraysRecursively;
+
+    /**
+     * @var ConfigurableOptionValueArrayMapper
+     */
+    private $arrayMapper;
+
+    /**
      * @inheritdoc
      */
     protected function setUp(): void
@@ -53,6 +66,10 @@ class ConfigurableProductOptionsTest extends StorefrontTestsAbstract
         $this->catalogService = Bootstrap::getObjectManager()->create(CatalogService::class);
         $this->productsGetRequestInterface = Bootstrap::getObjectManager()->create(ProductsGetRequestInterface::class);
         $this->productRepository = Bootstrap::getObjectManager()->create(ProductRepositoryInterface::class);
+        $this->catalogService = Bootstrap::getObjectManager()->create(CatalogService::class);
+        $this->productRepository = Bootstrap::getObjectManager()->create(ProductRepositoryInterface::class);
+        $this->arrayMapper = Bootstrap::getObjectManager()->create(ConfigurableOptionValueArrayMapper::class);
+        $this->compareArraysRecursively = Bootstrap::getObjectManager()->create(CompareArraysRecursively::class);
     }
 
     /**
@@ -76,5 +93,54 @@ class ConfigurableProductOptionsTest extends StorefrontTestsAbstract
 
         $catalogService = $catalogServiceItem->getItems()[0]->getConfigurableOptions()[0];
         $this->assertEquals($configurableOptions->getLabel(), $catalogService->getLabel());
+    }
+
+    /**
+     * Validate configurable product data
+     *
+     * @magentoDataFixture Magento/ConfigurableProduct/_files/configurable_product_with_two_child_products.php
+     * @magentoDbIsolation disabled
+     * @param array $expected
+     * @throws NoSuchEntityException
+     * @throws \Throwable
+     * @dataProvider getConfigurableProductOptionProvider
+     */
+    public function testConfigurableProductOptions(array $expected)
+    {
+        $product = $this->productRepository->get(self::TWO_CHILD_SKU);
+
+        $this->productsGetRequestInterface->setIds([$product->getId()]);
+        $this->productsGetRequestInterface->setStore(self::STORE_CODE);
+        $this->productsGetRequestInterface->setAttributeCodes($this->attributesToCompare);
+        $catalogServiceItem = $this->catalogService->getProducts($this->productsGetRequestInterface);
+        $this->assertNotEmpty($catalogServiceItem->getItems());
+
+        $actual = [];
+        foreach ($catalogServiceItem->getItems()[0]->getConfigurableOptions() as $productOption) {
+            $actual[] = $this->arrayMapper->convertToArray($productOption);
+        }
+
+        $diff = $this->compareArraysRecursively->execute(
+            $expected,
+            $actual
+        );
+        self::assertEquals([], $diff, "Actual response doesn't equal expected data");
+    }
+
+    /**
+     * //TODO:  ask for data provider
+     * @return array
+     */
+    public function getConfigurableProductOptionProvider()
+    {
+        return [
+            [
+                [
+                    [
+                        'test' => 'test'
+                    ]
+                ]
+            ]
+        ];
     }
 }
