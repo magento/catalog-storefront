b1d82a60b0853c43d243e013e7f96d5d1153c3e9
ruslankostiv <rkostiv@adobe.com>
2020-10-01 14:08:38 -0500

    172 - Read Api custom options testing

--- app/code/Magento/CatalogStorefront/Test/Api/Product/ProductOptions/SelectedOptionsTest.php
+++ app/code/Magento/CatalogStorefront/Test/Api/Product/ProductOptions/SelectedOptionsTest.php
@@ -14,7 +14,6 @@ use Magento\CatalogStorefrontApi\Api\Data\ProductOptionValueArrayMapper;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\TestFramework\Helper\Bootstrap;
 use Magento\TestFramework\Helper\CompareArraysRecursively;
-use Magento\CatalogStorefrontApi\Api\Data\ProductVariantsGetRequest;
 
 /**
  * Test class for Select custom options
@@ -29,7 +28,7 @@ class SelectedOptionsTest extends StorefrontTestsAbstract
      * @var string[]
      */
     private $attributesToCompare = [
-        'product_options'
+        'product_options',
     ];
 
     /**
@@ -58,11 +57,6 @@ class SelectedOptionsTest extends StorefrontTestsAbstract
     private $compareArraysRecursively;
 
     /**
-     * @var ProductVariantsGetRequest
-     */
-    private $productVariantInterface;
-
-    /**
      * @inheritdoc
      */
     protected function setUp(): void
@@ -70,7 +64,6 @@ class SelectedOptionsTest extends StorefrontTestsAbstract
         parent::setUp();
         $this->catalogService = Bootstrap::getObjectManager()->create(CatalogService::class);
         $this->productsGetRequestInterface = Bootstrap::getObjectManager()->create(ProductsGetRequestInterface::class);
-        $this->productVariantInterface = Bootstrap::getObjectManager()->create(ProductVariantsGetRequest::class);
         $this->productRepository = Bootstrap::getObjectManager()->create(ProductRepositoryInterface::class);
         $this->arrayMapper = Bootstrap::getObjectManager()->create(ProductOptionValueArrayMapper::class);
         $this->compareArraysRecursively = Bootstrap::getObjectManager()->create(CompareArraysRecursively::class);
@@ -88,7 +81,7 @@ class SelectedOptionsTest extends StorefrontTestsAbstract
      */
     public function testSelectOptionData(array $expected): void
     {
-        //product option value
+        /** @var \Magento\Catalog\Api\Data\ProductInterface $product */
         $product = $this->productRepository->get('simple');
         $this->productsGetRequestInterface->setIds([$product->getId()]);
         $this->productsGetRequestInterface->setStore(self::STORE_CODE);
@@ -111,36 +104,6 @@ class SelectedOptionsTest extends StorefrontTestsAbstract
             $actual
         );
         self::assertEquals([], $diff, "Actual response doesn't equal expected data");
-    }
-
-    /**
-     * Test product variant
-     *
-     * @magentoDataFixture Magento/Catalog/_files/product_with_options.php
-     * @magentoDbIsolation disabled
-     * @param array $expected
-     * @throws NoSuchEntityException
-     * @throws \Throwable
-     * @dataProvider variantsProvider
-     */
-    public function testProductVariants()
-    {
-        $this->markTestSkipped("This test skipped due to: https://github.com/magento/catalog-storefront/issues/304
-        and https://github.com/magento/catalog-storefront/issues/27");
-
-        //product variants
-        $product = $this->productRepository->get('simple');
-        $this->productVariantInterface->setIds([$product->getId()]);
-        $this->productVariantInterface->setStoreId(self::STORE_CODE);
-        $this->productVariantInterface->setAttributeCodes($this->attributesToCompare);
-        $catalogServiceVariants = $this->catalogService->getProductVariants($this->productVariantInterface);
-        self::assertNotEmpty($catalogServiceVariants->getItems());
-
-        //TODO: check why this return empty array
-        foreach ($catalogServiceVariants->getItems()[0]->getProduct() as $variant) {
-            $price = $variant->getPrice();
-            //TODO: Ask for array mapper for variants
-        }
     }
 
     /**
@@ -168,7 +131,7 @@ class SelectedOptionsTest extends StorefrontTestsAbstract
                         'default' => false,
                         'image_url' => '',
                         'qty_mutability' => false,
-                        'qty' => (float) 0,
+                        'qty' => (float)0,
                         'info_url' => '',
                     ],
                     [
@@ -225,27 +188,8 @@ class SelectedOptionsTest extends StorefrontTestsAbstract
                         'qty' => (float)0,
                         'info_url' => '',
                     ],
-                ]
-            ]
-        ];
-    }
-
-    public function variantsProvider(): array
-    {
-        return [
-            [
-                [
-                    [
-                        'option_value_id' => '',
-                        'regular_price' => 1,
-                        'final_price' => '',
-                        'scope' => 'default'
-                    ],
-                    [
-
-                    ]
-                ]
-            ]
+                ],
+            ],
         ];
     }
 }
