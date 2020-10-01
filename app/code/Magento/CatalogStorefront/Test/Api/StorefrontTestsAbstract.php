d583f03618cfd369ba894d399952a9d5e97a2d5a
Serhii Kovalenko <ganster3012@gmail.com>
2020-10-01 18:30:02 +0300

    Mview patch update proposal
    --create a test for this

--- app/code/Magento/CatalogStorefront/Test/Api/StorefrontTestsAbstract.php
+++ app/code/Magento/CatalogStorefront/Test/Api/StorefrontTestsAbstract.php
@@ -95,7 +95,7 @@ abstract class StorefrontTestsAbstract extends TestCase
     protected function runTest()
     {
         $output = [];
-        exec("lsof -i:5672 | wc -l", $output);
+        exec("lsof -n -itcp | wc -l", $output);
         echo "\n" . '::test::' . "\n";
         print_r($output);
 
