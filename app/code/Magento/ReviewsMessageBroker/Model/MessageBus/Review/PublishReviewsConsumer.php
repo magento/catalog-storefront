8f26efc0bd6cd33307405124543c2ccf4719bdc3
Aleksejs Baranovs <alexbar199516@gmail.com>
2020-10-21 02:31:39 +0300

    Support for ratings & reviews in Storefront App - Reviews import

---
+++ app/code/Magento/ReviewsMessageBroker/Model/MessageBus/Review/PublishReviewsConsumer.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace Magento\ReviewsMessageBroker\Model\MessageBus\Review;
+
+use Magento\CatalogStorefrontApi\Api\Data\ImportReviewsRequestMapper;
+use Magento\CatalogStorefrontApi\Api\ProductReviewsServerInterface;
+use Magento\ReviewsMessageBroker\Model\FetchReviewsInterface;
+use Magento\ReviewsMessageBroker\Model\MessageBus\ConsumerEventInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Publish reviews into storage
+ */
+class PublishReviewsConsumer implements ConsumerEventInterface
+{
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var ImportReviewsRequestMapper
+     */
+    private $importReviewsRequestMapper;
+
+    /**
+     * @var ProductReviewsServerInterface
+     */
+    private $productReviewsServer;
+
+    /**
+     * @var FetchReviewsInterface
+     */
+    private $fetchReviews;
+
+    /**
+     * @param FetchReviewsInterface $fetchReviews
+     * @param ImportReviewsRequestMapper $importReviewsRequestMapper
+     * @param ProductReviewsServerInterface $productReviewsServer
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        FetchReviewsInterface $fetchReviews,
+        ImportReviewsRequestMapper $importReviewsRequestMapper,
+        ProductReviewsServerInterface $productReviewsServer,
+        LoggerInterface $logger
+    ) {
+        $this->fetchReviews = $fetchReviews;
+        $this->importReviewsRequestMapper = $importReviewsRequestMapper;
+        $this->productReviewsServer = $productReviewsServer;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute(array $entities, string $scope = null): void
+    {
+        $reviewsData = $this->fetchReviews->execute($entities);
+        $importRequest = $this->importReviewsRequestMapper->setData(['reviews' => $reviewsData])->build();
+        $importResult = $this->productReviewsServer->ImportProductReviews($importRequest);
+
+        if ($importResult->getStatus() === false) {
+            $this->logger->error(\sprintf('Reviews import is failed: "%s"', $importResult->getMessage()));
+        }
+    }
+}
---
+++ app/code/Magento/ReviewsStorefront/Model/ProductReviewsServer.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Magento\ReviewsStorefront\Model;
+
+use Magento\CatalogStorefront\Model\CatalogRepository;
+use Magento\CatalogStorefrontApi\Api\Data\CustomerProductReviewRequestInterface;
+use Magento\CatalogStorefrontApi\Api\Data\CustomerProductReviewResponseInterface;
+use Magento\CatalogStorefrontApi\Api\Data\DeleteReviewsRequestInterface;
+use Magento\CatalogStorefrontApi\Api\Data\DeleteReviewsResponseInterface;
+use Magento\CatalogStorefrontApi\Api\Data\ImportReviewArrayMapper;
+use Magento\CatalogStorefrontApi\Api\Data\ImportReviewsRequestInterface;
+use Magento\CatalogStorefrontApi\Api\Data\ImportReviewsResponseInterface;
+use Magento\CatalogStorefrontApi\Api\Data\ImportReviewsResponseInterfaceFactory;
+use Magento\CatalogStorefrontApi\Api\Data\ProductReviewCountRequestInterface;
+use Magento\CatalogStorefrontApi\Api\Data\ProductReviewCountResponseInterface;
+use Magento\CatalogStorefrontApi\Api\Data\ProductReviewRequestInterface;
+use Magento\CatalogStorefrontApi\Api\Data\ProductReviewResponseInterface;
+use Magento\CatalogStorefrontApi\Api\ProductReviewsServerInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class for retrieving & importing reviews data
+ */
+class ProductReviewsServer implements ProductReviewsServerInterface
+{
+    /**
+     * @var ImportReviewArrayMapper
+     */
+    private $importReviewArrayMapper;
+
+    /**
+     * @var ImportReviewsResponseInterfaceFactory
+     */
+    private $importReviewsResponseInterfaceFactory;
+
+    /**
+     * @var CatalogRepository
+     */
+    private $catalogRepository;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param ImportReviewArrayMapper $importReviewArrayMapper
+     * @param ImportReviewsResponseInterfaceFactory $importReviewsResponseInterfaceFactory
+     * @param CatalogRepository $catalogRepository
+     * @param LoggerInterface $logger
+     */
+    public function __construct(
+        ImportReviewArrayMapper $importReviewArrayMapper,
+        ImportReviewsResponseInterfaceFactory $importReviewsResponseInterfaceFactory,
+        CatalogRepository $catalogRepository,
+        LoggerInterface $logger
+    ) {
+        $this->importReviewArrayMapper = $importReviewArrayMapper;
+        $this->importReviewsResponseInterfaceFactory = $importReviewsResponseInterfaceFactory;
+        $this->catalogRepository = $catalogRepository;
+        $this->logger = $logger;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function ImportProductReviews(ImportReviewsRequestInterface $request): ImportReviewsResponseInterface
+    {
+        $response = $this->importReviewsResponseInterfaceFactory->create();
+
+        try {
+            $reviewsInElasticFormat = [];
+
+            foreach ($request->getReviews() as $review) {
+                $review = $this->importReviewArrayMapper->convertToArray($review);
+                // TODO change review_id to id in proto
+                $review['id'] = $review['review_id'];
+                unset($review['review_id']);
+                $reviewsInElasticFormat['review']['']['save'][] = $review;
+            }
+
+            $this->catalogRepository->saveToStorage($reviewsInElasticFormat);
+
+            $response->setMessage('Records imported successfully');
+            $response->setStatus(true);
+        } catch (\Throwable $exception) {
+            $response->setMessage($message = \sprintf('Cannot process reviews import: %s', $exception->getMessage()));
+            $response->setStatus(false);
+            $this->logger->error($message, ['exception' => $exception]);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function DeleteProductReviews(DeleteReviewsRequestInterface $request): DeleteReviewsResponseInterface
+    {
+        // TODO: Implement DeleteProductReviews() method.
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function GetProductReviews(ProductReviewRequestInterface $request): ProductReviewResponseInterface
+    {
+        // TODO: Implement GetProductReviews() method.
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function GetCustomerProductReviews(
+        CustomerProductReviewRequestInterface $request
+    ): CustomerProductReviewResponseInterface {
+        // TODO: Implement GetCustomerProductReviews() method.
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function GetProductReviewCount(
+        ProductReviewCountRequestInterface $request
+    ): ProductReviewCountResponseInterface {
+        // TODO: Implement GetProductReviewCount() method.
+    }
+}
