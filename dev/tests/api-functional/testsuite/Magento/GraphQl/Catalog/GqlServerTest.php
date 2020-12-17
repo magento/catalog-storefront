779b38b6b62438c0a81d78caff2d553c36ca9a4c
Michail Slabko <mslabko@magento.com>
2020-12-17 12:05:33 -0600

    fix test

--- dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/GqlServerTest.php
+++ dev/tests/api-functional/testsuite/Magento/GraphQl/Catalog/GqlServerTest.php
@@ -7,9 +7,8 @@ declare(strict_types=1);
 
 namespace Magento\GraphQl\Catalog;
 
-use Magento\TestFramework\TestCase\GraphQlAbstract;
 
-class GqlServerTest extends GraphQlAbstract
+class GqlServerTest extends \Magento\CatalogStorefront\Test\Api\StorefrontTestsAbstract
 {
     /**
      * @var \Magento\TestFramework\ObjectManager
@@ -23,6 +22,20 @@ class GqlServerTest extends GraphQlAbstract
 
     /**
      * @magentoApiDataFixture Magento/Catalog/_files/product_simple.php
+     *
+     * 1. execute Magento/Catalog/_files/product_simple.php
+     * 1.1 create product in Magent
+     * 2. trigger indexer "on save" for CatalogFeedIndexer
+     * 3. indexser triggers callback \Magento\CatalogExport\Model\Indexer\EntityIndexerCallback::execute
+     * 3. callback send into Message Bus message with product updae {id:x} RabiitMQ
+     * 4. Consumer in Message Broker accept message: \Magento\CatalogMessageBroker\Model\MessageBus\Product\ProductsConsumer::processMessage
+     * 4.1 Consumer send REST to Magento go get product data (collected recently by feed indexer)
+     * 4.2. Consumer send data to Catalog Service via gRPC
+     *
+     * call Query "productByID"
+     * 1. send request to "TESTS_BASE_URL" (overriden in phpunit.xml for GQL Server)
+     * 2. GQL Server accept query and
+     *   - go to Storefront Service to get Data \Magento\CatalogStorefront\Model\CatalogService::getProducts
      */
     public function testGrpcRequest()
     {
