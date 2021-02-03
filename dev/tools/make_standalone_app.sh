753640a425b80d7cd8d6363ef08e9b0c0c624dd2
ruslankostiv <rkostiv@adobe.com>
2021-02-03 11:48:53 -0600

    SFAPP-270: Create composer.json for standalone installation

--- dev/tools/make_standalone_app.sh
+++ dev/tools/make_standalone_app.sh
@@ -2,6 +2,5 @@
 
 cp composer.json.standalone composer.json
 cp app/etc/catalog-storefront/di.xml.standalone app/etc/catalog-storefront/di.xml
-cp app/etc/db_schema.xml.standalone app/etc/db_schema.xml
 cp app/storefront_catalog_product_autoload.php app/autoload.php
 cp app/storefront_catalog_product_bootstrap.php app/bootstrap.php
