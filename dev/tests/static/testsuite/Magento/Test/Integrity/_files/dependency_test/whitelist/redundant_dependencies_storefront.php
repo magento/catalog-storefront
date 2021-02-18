f0108b7ea3b4d172241b83aaf09767ae38042dd1
duhon <duhon@rambler.ru>
2021-02-18 10:30:03 -0600

    SFAPP-299:Move modules from services to Shared Libraries

---
+++ dev/tests/static/testsuite/Magento/Test/Integrity/_files/dependency_test/whitelist/redundant_dependencies_storefront.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+return [
+    'Magento\StorefrontCache' => [
+        'Magento\StorefrontFramework' => 'Magento\StorefrontFramework'
+    ],
+    'Magento\StorefrontDeploy' => [
+        'Magento\StorefrontFramework' => 'Magento\StorefrontFramework'
+    ],
+    'Magento\StorefrontDi' => [
+        'Magento\StorefrontFramework' => 'Magento\StorefrontFramework'
+    ],
+    'Magento\StorefrontStorage' => [
+        'Magento\StorefrontFramework' => 'Magento\StorefrontFramework'
+    ],
+    'Magento\ReviewsStorefront' => [
+        'Magento\StorefrontFramework' => 'Magento\StorefrontFramework'
+    ],
+    'Magento\CatalogStorefront' => [
+        'Magento\StorefrontFramework' => 'Magento\StorefrontFramework'
+    ],
+    'Magento\CatalogStorefrontConfig' => [
+        'Magento\StorefrontFramework' => 'Magento\StorefrontFramework'
+    ]
+];
